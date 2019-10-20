16001b44f8954619d2a972e6c77bacce1463a33f
Igman Difari <54891762+igmandifari@users.noreply.github.com>
2019-10-20 22:54:18 +0700

    OOP-MVC-Basic
    Internet baru lancar astaga :(

---
+++ oopmvc/app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php 
+
+class User_model {
+    private $nama = 'Igman Difari';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
---
+++ oopmvc/app/views/about/index.php
@@ -0,0 +1,6 @@
+<div class="container">
+    
+    <h1 class="mt-4">About Me</h1>
+    <p>Halo, nasaya adalah ma saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, seorang <?= $data['pekerjaan']; ?>.</p>
+
+</div>
